8c6779f6e26e0ea4eef040dd947ed50246658c09
New <nkuuee@bk.ru>
2024-02-14 15:22:18 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ job4j_elementary/src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int a = 2; a < 10; a++) {
+            for (int b = 1; b < 10; b++) {
+                System.out.println(a + "*" + b + "=" + a * b);
+            }
+        }
+    }
+}
+
+
