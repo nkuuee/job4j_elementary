aae1b120f7e9938929dfe392eb08e2064df03278
nkuuee <nkuuee@bk.ru>
2025-12-10 00:18:31 +0300

    4.1 Решение задачи [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a < 9) {
+            a = 1 * a;
+            a++;
+            System.out.println("1 * " + a + " = " + a);
+        }
+
+    }
+}
