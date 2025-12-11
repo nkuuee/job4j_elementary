272ae8c729ed995add678b73ce33313d2d921f3f
nkuuee <nkuuee@bk.ru>
2025-12-11 14:23:20 +0300

    4.2 Решение задачи [#159036]

---
+++ src/main/java/ru/job4j/Info.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import java.time.LocalDate;
+
+public class Info {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate);
+    }
+}
